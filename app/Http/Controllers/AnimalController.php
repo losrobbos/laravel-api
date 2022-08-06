514b27b0a79fffa1fed036f1e73da88e1b39945d
losrobbos <losrobbos@web.de>
2022-08-06 12:31:18 +0200

    Initial commit - DB Crud

---
+++ app/Http/Controllers/AnimalController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Animal;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AnimalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $animals = DB::select('select * from animals');
+        return $animals;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $body = $request->all();
+
+        $animals = DB::insert('insert into animals (name) values (?)', [$body["name"]]);
+        return $animals;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $animal = DB::select('select * from animals where id = (?)', [$id]);
+        return $animal;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        if($data["name"]) {
+            $animalUpdated = DB::update("update animals set name = (?) where id = (?)", [
+                $data["name"], $id
+            ]);
+            return $animalUpdated;
+        }
+        else {
+            return "Animal not found";
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $animalDeleted = DB::delete("delete from animals WHERE id = (?)", [$id]);
+        return $animalDeleted;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\AnimalController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::resource('animals', AnimalController::class);
+
+// Route::get('/animal', function (Request $request) {
+//     return "<div>Animals</div>";
+// });
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
